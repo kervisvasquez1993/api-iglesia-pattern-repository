19f1a3a2c53bda31515dc1256ca918d9958a46d9
kervisvasquez1993 <cowin0809@gmail.com>
2025-02-20 23:21:25 -0300

    :building_construction: add foldes in project

--- database/migrations/2025_02_21_020204_create_pages_table.php
+++ database/migrations/2025_02_21_020204_create_pages_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('pages', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->softDeletes();
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
