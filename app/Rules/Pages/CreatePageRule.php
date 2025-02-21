5d82e24f2b71e003891dc7db817e7dc338882383
kervisvasquez1993 <cowin0809@gmail.com>
2025-02-20 23:51:44 -0300

    :construction: add rules in folder for project patter repository

---
+++ app/Rules/Pages/CreatePageRule.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Rules\Pages;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class CreatePageRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        //
+    }
+}
